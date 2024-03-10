17a48289d8c8a0c2a07b7c6ba3655397f8ccd379
sticktostack <rajdeep6153@gmail.com>
2024-03-11 02:33:58 +0530

    second commit

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ let menuBtn = document.getElementById('menubutton')
 menuBtn.addEventListener('click',()=>{
     if(menuBtn.classList.contains('ri-menu-line')){
         menuBtn.classList.replace('ri-menu-line','ri-close-line')
+        document.querySelector('.right').style.display = 'flex'
     }else{
         menuBtn.classList.replace('ri-close-line','ri-menu-line')
+        document.querySelector('.right').style.display = 'none'
+
     }
 })
