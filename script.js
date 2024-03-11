2e1472d961014359b4ca81041cf354b6e8077771
sticktostack <rajdeep6153@gmail.com>
2024-03-12 01:48:38 +0530

    final output

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 let menuBtn = document.getElementById('menubutton')
+let resMenu = document.querySelector('.responsivenav')
 menuBtn.addEventListener('click',()=>{
     if(menuBtn.classList.contains('ri-menu-line')){
         menuBtn.classList.replace('ri-menu-line','ri-close-line')
-        document.querySelector('.right').style.display = 'flex'
+        resMenu.style.transform = 'translateX(0%)'
     }else{
         menuBtn.classList.replace('ri-close-line','ri-menu-line')
-        document.querySelector('.right').style.display = 'none'
-
+        resMenu.style.transform = 'translateX(100%)'
     }
 })
+console.log(resMenu);
